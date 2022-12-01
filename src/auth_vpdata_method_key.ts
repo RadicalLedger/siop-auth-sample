e97f51ba896866532aaa369393d3c2fe10b0ba1d
Vijay Shiyani <vshiyani@meeco.me>
2022-12-01 18:58:57 +1100

    testing siop sample

--- src/auth_vpdata_method_key.ts
+++ src/auth_vpdata_method_key.ts
@@ -1,79 +1,42 @@
-import * as DID_SIOP from 'did-siop'
-import { PARAMS, optionsWithClaims, tokenData } from "./params"
-
+import * as DID_SIOP from 'did-siop';
+import { optionsWithClaims, PARAMS } from './params';
 
 let siop_rp: any = undefined;
 let request: any;
 
-async function startProcess(){
-    console.log('startProcess');
-    
-    let keyResolv2020 = new DID_SIOP.Resolvers.KeyDidResolver('key', "@digitalbazaar/ed25519-verification-key-2020")
-    siop_rp = await DID_SIOP.RP.getRP(
-        PARAMS.KEY_RP.redirect_uri, // RP's redirect_uri
-        PARAMS.KEY_RP.did, // RP's did
-        PARAMS.KEY_RP.registration,
-        undefined,
-        [keyResolv2020]
-    )
-    console.log('Got RP instance ....');
-    siop_rp.addSigningParams(PARAMS.KEY_RP.sigining_key); // Private key
+async function startProcess() {
+  console.log('startProcess');
 
-    console.log('RP SigningParams added ...');
-    request = await siop_rp.generateRequest(optionsWithClaims);    
+  let keyResolv2020 = new DID_SIOP.Resolvers.KeyDidResolver(
+    'key',
+    '@digitalbazaar/ed25519-verification-key-2020'
+  );
 
-    console.log('Request generated ...', request);
+  siop_rp = await DID_SIOP.RP.getRP(
+    PARAMS.KEY_RP.redirect_uri, // RP's redirect_uri
+    PARAMS.KEY_RP.did, // RP's did, // RP's did
+    // 'did:web:did-web.godiddy.com:891dd1be-7703-4749-997f-4aaf48518ae7',
+    PARAMS.KEY_RP.registration,
+    undefined,
+    [keyResolv2020]
+  );
 
-    console.log('Moving to Provider side ...');
+  console.log('Got RP instance ....');
+  siop_rp.addSigningParams(PARAMS.KEY_RP.sigining_key); // Private key
+  //siop_rp.addSigningParams(`2pZzJP2VDAVEwHkZf8spoVJfsMAoN5TiEhMmxBDF4tb7pgdM8`); // Private key
 
-    let siopTokenEncoded = await OnExtension(request);    
-    
-    let siopTokenObjects = await siop_rp.validateResponseWithVPData(siopTokenEncoded, {
-        redirect_uri: PARAMS.KEY_RP.redirect_uri,
-        isExpirable: true,
-        nonce: optionsWithClaims.nonce,
-    })
+  console.log('RP SigningParams added ...');
+  request = await siop_rp.generateRequest(optionsWithClaims);
 
-    console.log('Response validated...');
-    console.log('Validated response',siopTokenObjects); 
-}
+  console.log(request);
 
-async function OnExtension(request:string):Promise<DID_SIOP.SIOPTokensEcoded>{
-    let keyResolv2020 = new DID_SIOP.Resolvers.KeyDidResolver('key', "@digitalbazaar/ed25519-verification-key-2020")
-    const provider = await DID_SIOP.Provider.getProvider(PARAMS.KEY_USER.did,undefined,[keyResolv2020]);
-
-    return new Promise(async (resolve, reject) => {
-        console.log('User DID set to Provider ...');
-    
-        provider.addSigningParams(PARAMS.KEY_USER.sigining_key); // User's private key
-        
-        console.log('User SigningParams added ...');
-
-        // Request validation and response generation
-        provider.validateRequest(request).then(async (decodedRequest) => {
-            console.log('Request validation completed ...');
-            console.log('decodedRequest',decodedRequest);
-            let jwtExpiration = 5000;
-            try {
-                let vps: DID_SIOP.VPData = {
-                    vp_token : tokenData.good.singleVP.vp_token,
-                    _vp_token : tokenData.good.singleVP.id_token._vp_token
-                };
-                let siopTokenEncoded = await provider.generateResponseWithVPData(decodedRequest.payload, 5000,vps);
-                console.log('Response generated ...');
-                console.log('responseJWT',siopTokenEncoded); 
-                resolve(siopTokenEncoded);
-            }
-            catch (err){
-                console.log("provider.generateResponse Error " , err);
-            }
-        })
-        .catch(err => {
-          console.log("invalid request" , err);
-          reject(err);
-        })            
-
-    } );
+  const url = new URL(request);
+  const jwtWithoutSignature = url.searchParams
+    .get('request')
+    ?.split('.')
+    .slice(0, -1)
+    .join('.');
+  console.log('Request generated ...', jwtWithoutSignature);
 }
 
 console.log('on the main function');
